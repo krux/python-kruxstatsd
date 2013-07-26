494c1f16801a860d8665efb54a8f5403d711d0bd
Paul Lathrop <paul@tertiusfamily.net>
2013-07-26 12:59:31 -0700

    Refactor package metadata in preparation for internal pypi release.

--- setup.py
+++ setup.py
@@ -1,19 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# © 2013 Krux Digital, Inc.
+#
+
+from pip.req    import parse_requirements
 from setuptools import setup, find_packages
+
+import os
+
+
+# We use the version to construct the DOWNLOAD_URL.
+VERSION      = '0.2.0'
+
+# URL to the repository on Github.
+REPO_URL     = 'https://github.com/krux/python-kruxstatsd'
+
+# Github will generate a tarball as long as you tag your releases, so don't
+# forget to tag!
+DOWNLOAD_URL = ''.join((REPO_URL, '/tarball/release/', VERSION))
+
+# We want to install all the dependencies of the library as well, but we
+# don't want to duplicate the dependencies both here and in
+# requirements.pip. Instead we parse requirements.pip to pull in our
+# dependencies.
+BASE_DIR     = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS = os.path.join(BASE_DIR, 'requirements.pip')
+
+# A requirement file can contain comments (#) and can include some other
+# files (--requirement or -r), so we need to use pip's parser to get the
+# final list of dependencies.
+DEPENDENCIES = [unicode(package.req)
+                for package in parse_requirements(REQUIREMENTS)]
 
 
 setup(
-    name='kruxstatsd',
-    version='0.2.0',
-    description='A wrapper around pystatsd that does automatic namespacing',
-    packages=find_packages(),
-    author='Paul Osman',
-    author_email='paul@kruxdigital.com',
-    include_package_data=True,
-    package_data={'': ['README.rst']},
-    install_requires=[
-        'statsd==0.3.0',
-    ],
-    tests_require=[
+    name                 = 'kruxstatsd',
+    version              = VERSION,
+    author               = 'Paul Osman',
+    author_email         = 'paul@kruxdigital.com',
+    maintainer           = 'Paul Lathrop',
+    maintainer_email     = 'paul@krux.com',
+    description          = 'Wrapper around pystatsd with automatic namespacing',
+    url                  = REPO_URL,
+    download_url         = DOWNLOAD_URL,
+    license              = 'MIT',
+    packages             = find_packages(),
+    install_requires     = DEPENDENCIES,
+    tests_require        = [
         'nose==1.1.2',
         'fudge==1.0.3'
     ]
