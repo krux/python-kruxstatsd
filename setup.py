e8f465f3f6c437e0e5c630f829bbbca7cbfc4be4
Peter Han <phan@krux.com>
2016-09-02 19:08:49 -0700

    Simple linting of setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ import os
 
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION      = '0.2.4'
+VERSION = '0.3.0'
 
 # URL to the repository on Github.
-REPO_URL     = 'https://github.com/krux/python-kruxstatsd'
+REPO_URL = 'https://github.com/krux/python-kruxstatsd'
 
 # Github will generate a tarball as long as you tag your releases, so don't
 # forget to tag!
@@ -20,20 +20,20 @@ DOWNLOAD_URL = ''.join((REPO_URL, '/tarball/release/', VERSION))
 
 
 setup(
-    name                 = 'kruxstatsd',
-    version              = VERSION,
-    author               = 'Paul Osman',
-    maintainer           = 'Paul Lathrop',
-    maintainer_email     = 'paul@krux.com',
-    description          = 'Wrapper around pystatsd with automatic namespacing',
-    url                  = REPO_URL,
-    download_url         = DOWNLOAD_URL,
-    license              = 'MIT',
-    packages             = find_packages(),
-    install_requires     = [
+    name='kruxstatsd',
+    version=VERSION,
+    author='Paul Osman',
+    maintainer='Paul Lathrop',
+    maintainer_email='paul@krux.com',
+    description='Wrapper around pystatsd with automatic namespacing',
+    url=REPO_URL,
+    download_url=DOWNLOAD_URL,
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
         'statsd==2.0.3',
     ],
-    tests_require        = [
+    tests_require=[
         'nose==1.1.2',
         'fudge==1.0.3'
     ]
